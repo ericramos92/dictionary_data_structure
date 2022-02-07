63c9606b8861534bdfa30153e4218ccb2644876c
EricRamos <ericramosgarnica1@gmail.com>
2022-02-07 15:44:05 -0800

    adding random function

--- Capital_Game/capital_game.py
+++ Capital_Game/capital_game.py
@@ -1,6 +1,9 @@
+import random
 dic = {'Alabama':'Montgomery', 'Alaska': 'Juneau', 'Arizona': '	Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': '	Denver ', 'Connecticut': 'Hartford', 'Delaware': '	Dover', 'Florida': 'Tallahassee ', 'Massachusetts': 'Boston',
        'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge', 'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Michigan': 'Lansing',
        'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh', 'North Dakota': 'Bismarck',
        'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City', 'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence', 'South Carolina': '	Columbia', 'South Dakota': 'Pierre', 'Tennessee': 'Nashville', 'Texas': '	Austin', 'Utah': 'Salt Lake City', 'Vermont': 'Montpelier', 'Virginia': 'Richmond',
        'Washington': 'Olympia', 'West Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'
        }
+
+print(random.choice(list(dic.keys())))
